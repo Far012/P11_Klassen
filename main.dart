edd4e2f674506b5a60626acebc9adae0ce92ff01
Farhad <farhad.ahmadi@outlook.de>
2025-09-02 14:03:52 +0200

    init

---
+++ main.dart
@@ -0,0 +1,36 @@
+class CardAppointment {
+  String titel;
+  String description;
+  String schoolClass;
+  DateTime date;
+  String startTime;
+  String endTime;
+  String location;
+  String detailsUrl;
+  bool isDone;
+
+  CardAppointment(this.titel, this.description, this.schoolClass, this.date, this.startTime, this.endTime, this.location, this.detailsUrl, this.isDone);
+}
+
+class CardHomework {
+  String subjectImage;
+  String titel;
+  String description;
+  String schoolClass;
+  DateTime dueDate;
+  String detailsUrl;
+  bool isDone;
+
+  CardHomework(this.subjectImage, this.titel, this.description, this.schoolClass, this.dueDate, this.detailsUrl, this.isDone);
+}
+
+class HomeworkDetails {
+  String schoolSubject;
+  String titel;
+  String description;
+  List <String> attachments;
+  String headlineSubmissionDate;
+  DateTime dueDate;
+
+  HomeworkDetails(this.schoolSubject, this.titel, this.description, this.attachments, this.headlineSubmissionDate, this.dueDate)
+}
